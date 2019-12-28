f3129a6b10848c0df3c3bc93aab4096a0844ae4b
Alex Tilatti <alex.tilatti@gmail.com>
2019-12-27 19:32:47 -0500

    working on adding daylist items for the entire day list

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,10 +2,18 @@ import React from "react";
 import DayListItem from "components/DayList";
 
 export default function DayList(props) {
-    const items = props.days.map((day) => 
-        <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay} />
-    );
-    return (
-      <ul> {items} </ul>
-    )
+  // const items = props.days.map(day => (
+  //     <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay} />
+  // ));
+  return (
+    <ul>
+      {
+        props.days.map(day => (
+          <li>
+            <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay} />
+          </li>
+        ))
+      }
+    </ul>
+  )
 }
