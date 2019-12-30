80f16a5617d7302ae71be711685bd349e581282a
Alex Tilatti <alex.tilatti@gmail.com>
2019-12-29 23:46:05 -0500

    added changes to set interviewers and the interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,12 +4,14 @@ import InterviewerListItem from "components/InterviewerListItem"
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map(interviewer => {
     return (
-      <section className="interviewers">
-        <h4 className="interviewers__header text--light">Interviewer</h4>
-        <ul className="interviewers__list"></ul>
-      </section>
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={event => props.setInterviewer(interviewer.id)}
+      />
     )
   })
 return <ul> {interviewers} </ul>
-  
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,20 @@
 import React from "react";
-i
+const classNames = require('classnames');
+
 export default function InterviewerListItem(props) {
+  let itemClass = classNames({
+    "interviewers__item--selected": props.selected,
+    "button--danger": props.danger
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={itemClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
-  )
+  );
 }
